e297535dc323e03a38b4eeaf150d4c1fb64ed5a1
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-05-18 22:15:06 +0930

    Add Jest support (with typescript)

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
+
+const add = (a: number, b: number): number => {
+  return a + b;
+}
+
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByText('Home');
+
+    expect(heading).toBeInTheDocument()
+  })
+})
+
+describe('Add', () => {
+  it('Adds nums', () => {
+    expect(add(1, 2)).toBe(3);
+  })
+})
